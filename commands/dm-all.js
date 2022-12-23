ff4613cbee05cef744d9413aade83872bbcda210
AmNobCop <96837273+AmNobCop@users.noreply.github.com>
2022-12-23 18:41:36 -0500

    Update dm-all.js
    Timeout so bot doesn't get flagged.

--- commands/dm-all.js
+++ commands/dm-all.js
@@ -9,12 +9,13 @@ module.exports = {
     .setDescription('DMs all users of a server'),
   async execute(interaction) {
     await interaction.deferReply();
+    setTimeout(async () => {
+      await interaction.guild.members.cache.forEach(async member => {
 
-    await interaction.guild.members.cache.forEach(async member => {
-
-      await member.send(dm_message).catch(error => {
-        console.log(`Error: ${error} | Couldn't send a message to ${member.tag}`)
-      })
+        await member.send(dm_message).catch(error => {
+          console.log(`Error: ${error} | Couldn't send a message to ${member.tag}`)
+        })
+      }, 2500)
     });
 
   },
